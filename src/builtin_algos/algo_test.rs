23a8ea74ae6c62844bcaa9512c79d5b081319e56
litchipi <litchi.pi@protonmail.com>
2021-05-27 12:41:26 +0200

    algo_test.rs: Adding TestAlgo2
    Signed-off-by: litchipi <litchi.pi@protonmail.com>

--- src/builtin_algos/algo_test.rs
+++ src/builtin_algos/algo_test.rs
@@ -66,6 +66,62 @@ impl Algo for TestAlgo{
     }
 }
 
+pub struct TestAlgo2{
+
+}
+
+impl Algo for TestAlgo2{
+    type CellType = TestCell;
+
+    fn new() -> Self where Self: Sized{
+        TestAlgo2 { }
+    }
+
+    fn reset(&mut self){
+
+    }
+
+    fn send_special_data(&self, params: &serde_json::Value) -> JsonData{
+        String::from("")
+    }
+
+    fn recv_special_data(&mut self, data: &serde_json::Value){
+
+    }
+
+    fn get_genome_length(&self) -> usize{
+        2
+    }
+
+    fn genome_from_json(&self, jsdata: JsonData) -> Genome{
+        __genome_from_json(jsdata, &KEY_LIST.to_vec())
+    }
+
+    fn genome_to_json(&self, genome: Genome) -> JsonData{
+        __genome_to_json(genome, &KEY_LIST.to_vec())
+    }
+
+    fn create_cell_from_genome(&self, genome: &Genome) -> Self::CellType{
+        TestCell {
+            celldata: CellData { genome: genome.clone(), score: 0.0, version: 1},
+        }
+    }
+
+    fn check_generation_over(&self, genalgo: &Lab<TestCell>) -> bool{
+        false
+    }
+
+    fn initialize_cells(&mut self, pop: &mut Vec<Self::CellType>){
+
+    }
+
+    fn process_data(&mut self, pop: &mut Vec<Self::CellType>, data: &GenalgoData){
+        for cell in pop.iter_mut(){
+            cell.action(&data);
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct TestCell{
     celldata: CellData,
